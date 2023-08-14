ffa9a51e28c02bfde164a6b3d502e23fc5af8f9b
Pogrammar <vatsal-lookout@outlook.com>
2023-08-14 19:20:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pygame
 from sys import exit
 
@@ -15,46 +16,91 @@ ground = pygame.image.load('graphics/ground.png')
 text = font.render('Runner', True, 'Black')
 
 
-snail = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail.get_rect(bottomright = (600, 300))
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+        self.rect = self.image.get_rect(midbottom = (80, 300))
+        self.gravity = 0
 
-player = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
-player_rect = player.get_rect(midbottom = (80, 300))
-player_gravity = 0
+        def player_input(self):
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.gravity = -20
+        def apply_gravity(self):
+           self.gravity += 1
+           self.rect.y += self.gravity
+
+        def update(self):
+            self.player_input()
+            self.apply_gravity()
+
+class Snail(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+        self.rect = self.image.get_rect(bottomright = (600, 300))
+
+    def respawn(self):
+        if self.rect.right <= 0:
+            self.rect.left = 800
+
+
+    def update(self):
+        self.rect.x -= 6
+        self.respawn()
+
+
+snail = pygame.sprite.GroupSingle()
+snail.add(Snail())
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+
+def collision():
+    if pygame.sprite.spritecollide(player.sprite,snail,False):
+        snail.empty()
+        return False
+    else: 
+        return True
+
+
+
+
 
 while True:
     for event in pygame.event.get(): #allows the user to exit the game
         if event.type == pygame.QUIT:
             pygame.quit()
             exit() #breaks the while loop, prevents conflict with pygame.init
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player_gravity = -20
+        if game_active:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+
+
     if game_active:
         #get screens onto respectinve cords from top left
         screen.blit(sky, (0, 0)) 
         screen.blit(ground,(0, 300))
         screen.blit(text, (320, 50))
         
-        #snail
-        snail_rect.x -= 4 #speed of snail moving
+        snail.draw(screen)
+        snail.update()
 
-        if snail_rect.right <= 0:
-            snail_rect.left = 800
+        player.draw(screen)
+        player.update()
 
-        screen.blit(snail, snail_rect)
+        game_active = collision()
 
-        #player
-        player_gravity += 1
-        player_rect.y += player_gravity
-
-        if player_rect.bottom >= 300:
-            player_rect.bottom = 300
-
-        screen.blit(player, player_rect)
-
-        if snail_rect.colliderect(player_rect):
-            game_active = False
+    else:
+        game_active = False
+        screen.fill((94,129,162)) 
+        gameover = font.render('Game Over - Press space to run', True, "Green")
+        screen.blit(gameover, (220, 50))
 
 
     pygame.display.update()
